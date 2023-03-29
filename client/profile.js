ab9e6dc45ce110808ac0692ef5107d3986892863
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-03-29 17:43:55 -0600

    got modal more functional

--- client/profile.js
+++ client/profile.js
@@ -55,10 +55,29 @@ form.addEventListener('submit', submitHandler)
 
 getAllPlants()
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+
+var modal = document.getElementById("myForm");
+
+// Get the button that opens the modal
+var btn = document.getElementById("open-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    console.log('ehy!')
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+}
